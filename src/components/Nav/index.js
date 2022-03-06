79b34b884971f31b781008edc647db92df85ad6e
adekoya samuel <adekoyasamuel52@gmail.com>
2022-03-07 00:57:06 +0100

    init

---
+++ src/components/Nav/index.js
@@ -0,0 +1,34 @@
+import logo from "./images/logo.svg"
+import hamburger from "./images/hamburger-menu.svg"
+import './style.css'
+import { useState } from "react"
+
+const Nav = () => {
+  const [show, setShow] = useState(false)
+  const showMenu = () => {
+    setShow(!show)
+  }
+  return (
+    <>
+      <div className="nav__container">
+        <img src={logo} alt="logo" className="logo" />
+        <div onClick={showMenu} className="hamburger__container">
+          <img src={hamburger} alt="menu" className="hamburger" />
+        </div>
+
+        {/* <div className="nav__links"> menu</div> */}
+      </div>
+      <div className={`nav__links ${show ? "nav__show" : null}`}>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+
+  )
+}
+
+export default Nav;
