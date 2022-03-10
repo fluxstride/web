119d16818ef18c102887aa53bb715b85842b9786
adekoya samuel <adekoyasamuel52@gmail.com>
2022-03-10 23:08:06 +0100

    -

--- src/components/Me/index.js
+++ src/components/Me/index.js
@@ -5,12 +5,16 @@ const Me = () => {
       <h1 className="me__title left">
         Me
       </h1>
-      <div className="rig">
+      <div className="right">
         <p className="me__text">
           My name is adekoya samuel and i am
           a software developer with proficient
           knowledige in Web Development.
         </p>
+        <div className="right__bottom">
+          <p>{"know more ->"}</p>
+        <div className="sph"></div>
+        </div>
       </div>
     </div>
   )
