46b5df5b0770e92461796ffa320bf5378bbc6883
adekoya samuel <adekoyasamuel52@gmail.com>
2022-03-10 22:34:15 +0100

    -

--- src/components/Me/index.js
+++ src/components/Me/index.js
@@ -1,8 +1,17 @@
-import './style.css' 
+import './style.css'
 const Me = () => {
   return (
     <div className="me">
-
+      <h1 className="me__title left">
+        Me
+      </h1>
+      <div className="rig">
+        <p className="me__text">
+          My name is adekoya samuel and i am
+          a software developer with proficient
+          knowledige in Web Development.
+        </p>
+      </div>
     </div>
   )
 }
